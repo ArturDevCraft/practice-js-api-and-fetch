17b3df8b261fece98250f7b89e2720d06fb51b25
Artur <artur.czerwiak@gmail.com>
2025-11-13 11:25:34 +0100

    Task 01 completed

--- 01/app.js
+++ 01/app.js
@@ -1,21 +1,24 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
+	const divList = document.querySelectorAll('div');
 
+	setBorderColorAsync(divList[0], 'red', firstCallback);
 }
 
 function setBorderColorAsync(element, color, callback) {
-    setTimeout(() => {
-        element.style.border = `3px solid ${color}`;
-        callback();
-    }, Math.random() * 3000);
+	setTimeout(() => {
+		element.style.border = `3px solid ${color}`;
+		callback();
+	}, Math.random() * 3000);
+}
+
+function firstCallback() {
+	const divList = document.querySelectorAll('div');
+	setBorderColorAsync(divList[1], 'blue', secondCallback);
+}
+
+function secondCallback() {
+	const divList = document.querySelectorAll('div');
+	setBorderColorAsync(divList[2], 'green', () => console.log('Finished'));
 }
