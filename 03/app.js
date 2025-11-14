4dd5e59a55d2adaa556d475f162b5b907d217b05
Artur <artur.czerwiak@gmail.com>
2025-11-14 09:20:57 +0100

    Task 03 completed

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,33 @@
 document.addEventListener('DOMContentLoaded', init);
 
-function init() {
-    console.log('DOM');
+async function init() {
+	console.log('DOM');
+	button();
+}
+
+async function button() {
+	const button = document.querySelector('button');
+	const ipField = document.querySelector('span');
+	try {
+		const ip = await getIp();
+		button.addEventListener('click', () => {
+			ipField.innerText = ip;
+		});
+	} catch (err) {
+		ipField.innerText = err.message;
+	}
+}
+
+async function getIp() {
+	const URL = 'https://api.ipify.org?format=json';
+	try {
+		const response = await fetch(URL);
+		if (!response.ok) {
+			throw new Error('Something went wrong!!!');
+		}
+		const data = await response.json();
+		return data.ip;
+	} catch (err) {
+		throw new Error('Connection problem');
+	}
 }
